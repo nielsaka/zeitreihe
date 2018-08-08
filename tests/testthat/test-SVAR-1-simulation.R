3e827f7225bce8e9c00a841d87c6fb79b7968d69
Niels Aka <nielsaka@outlook.com>
2018-08-08 10:52:49 +0200

    add further test for SVAR synth data
    recover B matrix and compare
    tests pass

--- tests/testthat/test-SVAR-1-simulation.R
+++ tests/testthat/test-SVAR-1-simulation.R
@@ -11,9 +11,21 @@ W <- matrix(rnorm(N * K), nrow = K, ncol = N)
 
 test_that("Simulating SVAR works", {
   # pin down values
-  expect_equal_to_reference(create_svar_data(A, B, Y0, W), "svar_data.rds")
+  out <- create_svar_data(A, B, Y0, W)
+  expect_equal_to_reference(out, "svar_data.rds")
 
   # estimation? choleski, reduced-form
+  y <- out[, (3:N) + p]
+  y_1 <- out[, (3:N) + p - 1]
+  y_2 <- out[, (3:N) + p - 2]
 
+  fma <- ~ y_1[1, ] + y_1[2, ] + y_1[3, ] + y_2[1, ] + y_2[2, ] + y_2[3, ]  - 1
+  resid1 <- lm(update(fma, y[1, ] ~ .))$resid
+  resid2 <- lm(update(fma, y[2, ] ~ .))$resid
+  resid3 <- lm(update(fma, y[3, ] ~ .))$resid
 
+  rr <- cbind(resid1, resid2, resid3)
+  covmat <- t(rr) %*% rr / (N - K)
+  expect_equivalent(t(chol(covmat)), B, tol = 2E-2)
+  # TODO: move to other test file and compare with chol_decomp(covmat)
 })
