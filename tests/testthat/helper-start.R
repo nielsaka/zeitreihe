ca334e8b86ebe5b0b3fce809aa1e0fd6c62b3d6a
Niels Aka <nielsaka@outlook.com>
2018-08-10 17:18:31 +0200

    add test of covariance matrix
    at first thought that there is an error in computation, because `N` is not the actual sample size. However, was already taken into account by subtracting lag length `p`.

--- tests/testthat/helper-start.R
+++ tests/testthat/helper-start.R
@@ -1,1 +1,1 @@
-save_time <- sample(c(FALSE, rep(TRUE, 20)), 1)
+save_time <- sample(c(FALSE, rep(TRUE, 4)), 1)
--- tests/testthat/test-VAR-2-estimation.R
+++ tests/testthat/test-VAR-2-estimation.R
@@ -10,12 +10,27 @@ test_that("Simple multivariate OLS succeeds", {
 
   skip_if(save_time)
 
+  # test SIGMA.hat
   K <- 2
-  N <- 1E6
+  N <- 1E1
+  p <- 2
+  reps <- 2E3
+
+  set.seed(8191)
+  input <- replicate(reps, prep_input_varp(K, N, p, seed = sample(1E5, 1)))
+  Y <- lapply(seq_len(reps), function(x) do.call("create_varp_data", input[, x]))
+  out <- lapply(Y, ols_mv, p = p)
+  SIGMA.hat <- rowMeans(sapply(out, function(x) x$SIGMA.hat))
+
+  expect_equal(SIGMA.hat, c(1, 0, 0, 1), tol = 2E-2)
+
+  # test BETA.hat
+  K <- 2
+  N <- 2E5
   p <- 3
 
   Y <- do.call("create_varp_data", prep_input_varp(K, N, p))
   out <- ols_mv(Y = Y, p)
-  expect_equivalent(out$BETA.hat[, -1], make_A(K, p), tol = 3E-3)
+  expect_equivalent(out$BETA.hat[, -1], make_A(K, p), tol = 4E-3)
 })
 
