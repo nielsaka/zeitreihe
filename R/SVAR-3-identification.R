a716bdf5d4609116d22101d04c17820f86c94af0
Niels Aka <nielsaka@outlook.com>
2019-09-01 15:39:23 +0200

    remove duplicate code (SVAR identification)

--- R/SVAR-3-identification.R
+++ R/SVAR-3-identification.R
@@ -16,18 +16,6 @@
 
 ###############################################################################.
 
-amat <- matrix(
-  c(
-    1, 0, 0.5,
-    0.5, 1, 0,
-    0, 0.5, 1
-  ), ncol = 3, byrow = TRUE
-)
-# qr(amat)$rank # full rank of B matrix not sufficient!
-# rank_B_model(amat)
-# is_identifiable(amat)
-# solve(amat)
-
 is_identifiable <- function(A = NULL, B = NULL) {
 
   has_A <- !is.null(A)
@@ -116,27 +104,6 @@ rank_B_model <- function(B) {
 # K <- 10
 # all(duplication_sequence(K) == dupl_sequence_2(K))
 
-
-## -----> identification !?
-
-B = matrix(
-  c(1, 0.5, 0,
-    -0.2, 1, 0,
-    0.8, 0, 1),
-  3, 3, byrow = TRUE
-)
-
-selection_matrix <- function(Z) {
-  is_zero <- vec(Z) == 0
-
-  K <- nrow(Z)
-  N <- sum(is_zero)
-
-  res <- matrix(0, N, K^2)
-  res[cbind(seq_len(N), which(is_zero))] <- 1
-  return(res)
-}
-
 # CB <- selection_matrix(B)
 
 A <- matrix(
@@ -151,19 +118,7 @@ A <- matrix(
 # selection_matrix(A)
 
 
-
-duplication_matrix_ginverse <- function(K) {
-  #D <- duplication_matrix(K)
-  # solve(t(D) %*% D) %*% t(D)
-  MASS::ginv(duplication_matrix(K))
-}
-
 # D_plus(4)
-
-
-## ----> in package "time_tools"
-
-
 
 
 ##################################.
@@ -194,11 +149,7 @@ is_identifiable_graphic <- function(B) {
 
 # is_identifiable_graphic(B)
 
-
-
-
-
-
+###############################################################################.
 
 amat <- matrix(
   c(
@@ -238,50 +189,3 @@ B = matrix(
     0.8, 0, 1),
   3, 3, byrow = TRUE
 )
-
-selection_matrix <- function(B) {
-  is_zero <- vec(B) == 0
-
-  K <- nrow(B)
-  N <- sum(is_zero)
-
-  res <- matrix(0, N, K^2)
-  res[cbind(seq_len(N), which(is_zero))] <- 1
-  return(res)
-}
-# CB <- selection_matrix(B)
-
-D_plus <- function(K) {
-  D <- duplication_matrix(K)
-  solve(t(D) %*% D) %*% t(D)
-}
-# D_plus(4)
-
-
-## ----> in package "time_tools"
-
-##################################.
-# Graphical
-##################################.
-
-# no E vertex may have same set of children
-
-is_identifiable_graphic <- function(B) {
-  # is graph disconnected?
-  del <- which(rowSums(abs(B)) == 1 & colSums(abs(B)) == 1)
-  if (length(del)) B <- B[-del, -del]
-
-  K <- nrow(B)
-  if (sum(B == 0) < (K * (K - 1) / 2)) return(FALSE)
-
-
-  for (col in seq_len(K)) {
-    i <- 1
-    while(col + i <= K) {
-      fail <- all((B[, col] != 0) == (B[, col + i] != 0))
-      if (fail) return(FALSE)
-      i <- i + 1
-    }
-  }
-  return(TRUE)
-}
